141865614cbad5fa7cdff39af43b8f336a5fcadf
Tayla van Ingen <60573737+TaylavanIngen@users.noreply.github.com>
2020-03-25 13:11:10 +0100

    update W2D3-opdracht1

---
+++ Alle_Opdrachten/Week_2/W2D3/W2D3-opdracht1/opdracht1.js
@@ -0,0 +1,18 @@
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+}
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+() => 1 + 2;
+
+const myFunction = (a,b) => a + b
+
+const addFive = a => a + 5
+
+
+const createObject = () => ({greeting: "hoi"})
